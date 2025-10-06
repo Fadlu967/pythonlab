115b0b1fe156c6098547478b4ddeef3797c38539
Fadlu967 <fadlurahman705@gmail.com>
2025-10-06 19:18:42 +0530

    Update fibunacci.py

--- pythonlab/06-10-2004/fibunacci.py
+++ pythonlab/06-10-2004/fibunacci.py
@@ -1,19 +1,17 @@
-def fib(n):
-    a = 0
-    b = 1
-    if n==1:
-        print(a)
-    elif n==2:
-        print(a, "\n", b)
-    else:
-        print(a)
-        for i in range(2, n+1):
-            a += b
-            b = a - b
-            print(a)
-            
 d = int(input("Enter the limit : "))
+if d < 1:
+    print("Invalid input!")
+else:
+    print(f"First {d} Fibonacci numbers are : ")
+    def fib(n):
+        a = 0
+        b = 1
+        if n==1:
+            print(a)
+        else:
+            print(a)
+            for i in range(n-1):
+                a += b
+                b = a - b
+                print(a)
 fib(d)
-            
-            
-        
